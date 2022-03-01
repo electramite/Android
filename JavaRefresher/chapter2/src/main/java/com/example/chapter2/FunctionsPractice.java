b5c519f595adedcfd2e6de7c41416c8f3d4eb655
shashwat <electramite@gmail.com>
2022-03-01 21:30:23 +0530

    java practice

---
+++ JavaRefresher/chapter2/src/main/java/com/example/chapter2/FunctionsPractice.java
@@ -0,0 +1,10 @@
+package com.example.chapter2;
+
+public class FunctionsPractice {
+    public double avg(int a, int b, int c){
+        return (a+b+c)/3.0;
+    }
+    public void main(String[] args) {
+        System.out.println(avg(1,2,3));
+    }
+}
---
+++ JavaRefresher/chapter2/src/main/java/com/example/chapter2/MyClass.java
@@ -0,0 +1,11 @@
+package com.example.chapter2;
+
+public class MyClass {
+    public static void main(String[] args) {
+        String name = "shashwat patel";
+        System.out.println("Hello "+name);
+        boolean t = true;
+        FunctionsPractice t1 = new FunctionsPractice();
+        System.out.println(t1.avg(1,2,3));
+    }
+}
