46c3877920b4246750924a4c7a1b84a7eede5841
shashwat <electramite@gmail.com>
2022-03-11 19:05:40 +0530

    justJAVA

--- JustjAVA/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustjAVA/app/src/main/java/com/example/justjava/MainActivity.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
+    int q =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +18,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void submitOrder(View view){
-        display(1);
+        if(q==0){
+            Toast.makeText(this, "Add More Coffee", Toast.LENGTH_SHORT).show();
+            displayPrice(0);
+        }
+        else{
+            displayPrice(q*5);
+            Toast.makeText(this, "Order Placed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void increment(View view){
+        q++;
+        display(q);
+    }
+    public void decrement(View view){
+        if(q>0) q--;
+        display(q);
     }
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show();
         quantityTextView.setText("" + number);
+    }
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
