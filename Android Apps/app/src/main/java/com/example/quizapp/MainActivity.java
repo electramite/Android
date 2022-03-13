daad994f6393361e69b68bf18b6dcfe6452aa15b
shashwat <electramite@gmail.com>
2022-03-13 23:41:51 +0530

    main

--- Android Apps/app/src/main/java/com/example/quizapp/MainActivity.java
+++ Android Apps/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button falseButton;
     private Button trueButton;
     private TextView question;
+    private ImageButton nextButton;
+    private int qId = 0;
 
     private Questions[] questionBank = new Questions[] {
             new Questions(R.string.test_question1, false),
@@ -30,23 +33,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nextButton = findViewById(R.id.next_text);
         falseButton = findViewById(R.id.false_button);
         trueButton = findViewById(R.id.true_button);
         question = findViewById(R.id.answer_text_view);
         falseButton.setOnClickListener(this);
         trueButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.false_button:
-                Toast.makeText(this, "False", Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
                 break;
             case R.id.true_button:
-                Toast.makeText(this, "True", Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
                 break;
+            case R.id.next_text:
+                //Go on next question
+                qId = (qId+1)%questionBank.length;
+                question.setText(questionBank[qId].getAnswerResId());
+                
         }
 
+    }
+    public void checkAnswer(boolean userChoise){
+        boolean correctAnswer = questionBank[qId].isAnswerTrue();
+        if(correctAnswer == userChoise){
+            Toast.makeText(this, R.string.correct_ans, Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, R.string.incorrect_ans, Toast.LENGTH_SHORT).show();
+        }
     }
 }
