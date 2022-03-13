2bf099a72c898cad356afd973d7020c51794b6f7
shashwat <electramite@gmail.com>
2022-03-13 12:01:25 +0530

    main

--- Android Apps/app/src/main/java/com/example/quizapp/MainActivity.java
+++ Android Apps/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button falseButton;
     private Button trueButton;
     private TextView question;
@@ -18,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
         falseButton = findViewById(R.id.false_button);
         trueButton = findViewById(R.id.true_button);
         question = findViewById(R.id.answer_text_view);
+        falseButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.false_button:
+                Toast.makeText(this, "False", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.true_button:
+                Toast.makeText(this, "True", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
