5304ce2242e9592e6857288b83398e0c9d960a4f
shashwat <electramite@gmail.com>
2022-03-19 20:53:15 +0530

    main

--- Android Apps/SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ Android Apps/SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.sharedpreferences;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.dbText);
+        editText = findViewById(R.id.edText);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
