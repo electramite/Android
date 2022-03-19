ac496186f11bff058b711e66978db98c2725873e
shashwat <electramite@gmail.com>
2022-03-20 02:10:09 +0530

    main

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -9,10 +11,51 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WordAdapter extends ArrayAdapter<Word>{
-    public WordAdapter(@NonNull Context context, @NonNull List<Word> objects) {
-        super(context, resource, objects);
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param words A List of AndroidFlavor objects to display in a list
+     */
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item_layout, parent, false);
+        }
+        // extracting current translation for the position
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView mTranslation = listItemView.findViewById(R.id.miwok);
+        mTranslation.setText(currentWord.getMiWokTranslation());
+        TextView dTranslation = listItemView.findViewById(R.id.engNums);
+        dTranslation.setText(currentWord.getmDefaultTranslation());
+
+        return listItemView;
     }
 }
