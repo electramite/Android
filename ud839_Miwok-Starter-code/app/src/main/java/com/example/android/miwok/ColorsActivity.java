3ddf25681db372ecf6abe3e4a7af68dd729e598b
shashwat <electramite@gmail.com>
2022-03-20 21:26:24 +0530

    main

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -3,6 +3,9 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+        ArrayList<Word> colors = new ArrayList<Word>();
+        colors.add(new Word("red", "weṭeṭṭi"));
+        colors.add(new Word("green", "chokokki")); //
+
+
+        colors.add(new Word("three", "tolookosu"));
+        colors.add(new Word("four", "oyyisa"));
+        colors.add(new Word("five", "massokka"));
+        colors.add(new Word("six", "temmokka"));
+        colors.add(new Word("seven", "kenekaku"));
+        colors.add(new Word("eight", "kawinta"));
+        colors.add(new Word("nine", "wo’e"));
+        colors.add(new Word("ten", "na’aacha"));
+
+        WordAdapter adapter = new WordAdapter(this, colors);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
